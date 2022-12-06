7f8729503f8a86f1efd84b741842bb5358837b6b
Eric C Huang <eric_c_huang@trendmicro.com>
2022-12-06 14:20:30 +0800

    done part 2

--- day06/test_main.py
+++ day06/test_main.py
@@ -4,7 +4,7 @@ import pytest
 
 
 def check_start_packet(marker, max_length=4):
-    for i in range(len(marker) - 4):
+    for i in range(len(marker) - max_length):
         packet = set(marker[i : i + max_length])
         if len(packet) == max_length:
             return i + max_length
@@ -13,17 +13,22 @@ def check_start_packet(marker, max_length=4):
 
 
 @pytest.mark.parametrize(
-    "marker,expected",
+    "marker,expected,max_length",
     [
-        ('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7),
-        ('bvwbjplbgvbhsrlpgdmjqwftvncz', 5),
-        ('nppdvjthqldpwncqszvftbrmjlhg', 6),
-        ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10),
-        ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11),
+        ('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7, 4),
+        ('bvwbjplbgvbhsrlpgdmjqwftvncz', 5, 4),
+        ('nppdvjthqldpwncqszvftbrmjlhg', 6, 4),
+        ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10, 4),
+        ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11, 4),
+        ('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 19, 14),
+        ('bvwbjplbgvbhsrlpgdmjqwftvncz', 23, 14),
+        ('nppdvjthqldpwncqszvftbrmjlhg', 23, 14),
+        ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 29, 14),
+        ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 26, 14),
     ],
 )
-def test_check_start_packet(marker, expected):
-    actual = check_start_packet(marker)
+def test_check_start_packet(marker, expected, max_length):
+    actual = check_start_packet(marker, max_length)
     assert actual == expected
 
 
@@ -34,3 +39,6 @@ if __name__ == '__main__':
         line = f.readline()
         result = check_start_packet(line)
         print(f"part 1: {result}")
+
+        result = check_start_packet(line, 14)
+        print(f"part 2: {result}")
