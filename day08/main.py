d8d938dd3f59d7827a20ae5f84cc6fab0d08845d
Eric C Huang <eric_c_huang@trendmicro.com>
2022-12-08 20:30:31 +0800

    done day 8

--- day08/main.py
+++ day08/main.py
@@ -20,7 +20,7 @@ def check_visible(trees, x, y):
         if result_up:
             return 1
 
-        # check_down
+        # check down
         result_down = True
         for i in range(x + 1, row_count):
             if trees[x][y] <= trees[i][y]:
@@ -53,6 +53,41 @@ def check_visible(trees, x, y):
     return 0
 
 
+def get_scenic_score(trees, x, y):
+    row_count = len(trees)
+    column_count = len(trees[0])
+
+    # check up
+    up_score = 0
+    for i in range(x - 1, -1, -1):
+        up_score += 1
+        if trees[x][y] <= trees[i][y]:
+            break
+
+    # check down
+    down_score = 0
+    for i in range(x + 1, row_count):
+        down_score += 1
+        if trees[x][y] <= trees[i][y]:
+            break
+
+    # check left
+    left_score = 0
+    for j in range(y - 1, -1, -1):
+        left_score += 1
+        if trees[x][y] <= trees[x][j]:
+            break
+
+    # check right
+    right_score = 0
+    for j in range(y + 1, column_count):
+        right_score += 1
+        if trees[x][y] <= trees[x][j]:
+            break
+
+    return up_score * down_score * left_score * right_score
+
+
 if __name__ == '__main__':
     filename = sys.argv[1]
 
@@ -61,11 +96,16 @@ if __name__ == '__main__':
         for line in f.readlines():
             trees.append([int(c) for c in line.strip()])
 
-    result = 0
+    total = 0
+    max = 0
     for i in range(len(trees)):
         for j in range(len(trees[i])):
-            result += check_visible(trees, i, j)
+            result_1 = check_visible(trees, i, j)
+            total += result_1
 
-    print(f"part 1: {result}")
+            result_2 = get_scenic_score(trees, i, j)
+            if result_2 > max:
+                max = result_2
 
-
+    print(f'part 1: {total}')
+    print(f'part 2: {max}')
